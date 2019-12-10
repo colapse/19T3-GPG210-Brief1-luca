2ada7fdc934a4fcbfb685cb1fe87b634236924ff
Luca <lucatannler@hotmail.com>
2019-12-11 09:45:18 +1100

    Camera Target Boundaries Update

--- 19T3-GPG210-Brief1-Prototypes/Assets/Scripts/Camera/CameraTargetBorder.cs
+++ 19T3-GPG210-Brief1-Prototypes/Assets/Scripts/Camera/CameraTargetBorder.cs
@@ -1,4 +1,5 @@
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using Cinemachine;
@@ -9,6 +10,7 @@ using UnityEngine;
 namespace Camera
 {
     
+    [ExecuteAlways]
     public class CameraTargetBorder : MonoBehaviour
     {
         public bool emptyAllFocusTargets;
@@ -20,9 +22,26 @@ namespace Camera
 
         public SlimeManager playerSlimeManager; // hacky
 
+        public Collider col;
+
+        
+        // Gizmos
+        public Sprite indicatorSprite;
+        private BoxCollider boxCollider;
+        private SphereCollider sphereCollider;
+        private MeshCollider meshCollider;
+        private delegate void DrawColliderDel();
+        private DrawColliderDel drawColliderGizmos;
+
+
         private void Start()
         {
             playerSlimeManager = FindObjectOfType<SlimeManager>();
+            drawColliderGizmos = DrawDefaultColliderGizmos;
+            if (col == null)
+                col = GetComponent<Collider>();
+            CheckColliderType();
+            
         }
 
         private void AddTargetToList()
@@ -53,6 +72,79 @@ namespace Camera
                 sct.RemoveFocusTargets(removeFocusTargets);
             if(addFocusTargets?.Count > 0)
                 sct.AddFocusTargets(addFocusTargets);
+        }
+
+        public bool drawGizmos = true;
+        public bool alwaysDrawGizmos = true;
+        
+        private void OnDrawGizmos()
+        {
+            if(!drawGizmos || !alwaysDrawGizmos)
+                return;
+            
+            Matrix4x4 cubeTransform = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
+            Matrix4x4 oldGizmosMatrix = Gizmos.matrix;
+ 
+            Gizmos.matrix = cubeTransform;
+ 
+            drawColliderGizmos?.Invoke();
+            Gizmos.matrix = oldGizmosMatrix;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if(!drawGizmos || alwaysDrawGizmos)
+                return;
+            
+            Matrix4x4 cubeTransform = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
+            Matrix4x4 oldGizmosMatrix = Gizmos.matrix;
+ 
+            Gizmos.matrix = cubeTransform;
+            drawColliderGizmos?.Invoke();
+            Gizmos.matrix = oldGizmosMatrix;
+        }
+
+        private void DrawBoxColliderGizmos()
+        {
+            Gizmos.color = new Color32(64, 223, 255,100);
+            
+            Gizmos.DrawCube(boxCollider.center, boxCollider.size);
+            Gizmos.DrawIcon(col.bounds.center, "Collab.BuildSucceeded", true);
+        }
+        
+        private SphereCollider sphereCol;
+
+        private void DrawSphereColliderGizmos()
+        {
+            Gizmos.color = new Color32(64, 223, 255,100);
+            Gizmos.DrawSphere(sphereCollider.center, sphereCollider.radius);
+            Gizmos.DrawIcon(col.bounds.center, "Collab.BuildSucceeded", true);
+        }
+
+        private void DrawDefaultColliderGizmos()
+        {
+            Gizmos.color = new Color32(64, 223, 255,100);
+            Gizmos.DrawIcon(col.bounds.center, "Collab.BuildSucceeded", true);
+        }
+
+        private void CheckColliderType()
+        {
+            if (col == null)
+                return;
+            switch (col)
+            {
+                case BoxCollider boxCol:
+                    drawColliderGizmos = DrawBoxColliderGizmos;
+                    boxCollider = boxCol;
+                    break;
+                case SphereCollider sCol:
+                    drawColliderGizmos = DrawSphereColliderGizmos;
+                    sphereCollider = sCol;
+                    break;
+                default:
+                    drawColliderGizmos = DrawDefaultColliderGizmos;
+                    break;
+            }
         }
     }
 }
